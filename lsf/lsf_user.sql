85d4ccf5c777efeac53d4e9bba40ae61cb11f8fc
Michele Clamp <mclamp@rclogin04.rc.fas.harvard.edu>
2012-06-15 14:08:45 -0400

    Added groups to mysql_loadusers

--- lsf/lsf_user.sql
+++ lsf/lsf_user.sql
@@ -11,16 +11,16 @@ drop table if exists `user_group`;
 
 create table `user_group` (
    `user_internal_id` int not null,
-   `group_internal_id` int not null,
+   `labgroup_internal_id` int not null,
 
-   primary key (user_internal_id,group_internal_id)
+   primary key (user_internal_id,labgroup_internal_id)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-drop table if exists `group`;
+drop table if exists `labgroup`;
 
-create table `group` (
-   `group_internal_id` int unique not null auto_increment,
-   `group_name` text(255) default null,
-   primary key (group_internal_id),
-   key (group_name(255))
+create table `labgroup` (
+   `labgroup_internal_id` int unique not null auto_increment,
+   `labgroup_name` text(255) default null,
+   primary key (labgroup_internal_id),
+   key (labgroup_name(255))
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
