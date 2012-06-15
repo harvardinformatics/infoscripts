47c381b2dd1faffe21dd3c1a6b88f901e525aef6
Michele Clamp <mclamp@rclogin04.rc.fas.harvard.edu>
2012-06-15 10:42:56 -0400

    Start of user creating script

--- lsf/lsf_user.sql
+++ lsf/lsf_user.sql
@@ -3,9 +3,24 @@ drop table if exists `user`;
 create table `user` (
    `user_internal_id` int unique not null auto_increment,
    `user_name` text(255) default null,
-   `group_name`     text(255) default null,
    primary key (user_internal_id),
-   key (user_name(255)),
-   key(group_name(255))
+   key (user_name(255))
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
+drop table if exists `user_group`;
+
+create table `user_group` (
+   `user_internal_id` int not null,
+   `group_internal_id` int not null,
+
+   primary key (user_internal_id,group_internal_id)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+drop table if exists `group`;
+
+create table `group` (
+   `group_internal_id` int unique not null auto_increment,
+   `group_name` text(255) default null,
+   primary key (group_internal_id),
+   key (group_name(255))
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
