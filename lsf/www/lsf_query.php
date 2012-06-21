d9d03cdad526a3636db42030d61a484da560f842
Michele Clamp <mclamp@camd12.rc.fas.harvard.edu>
2012-06-21 16:42:20 -0400

    Added readme, fixed buffer overflow, added new groupings, tweaks

--- lsf/www/lsf_query.php
+++ lsf/www/lsf_query.php
@@ -33,12 +33,14 @@ if (isset($_REQUEST['labgroup'])) {
 } else {
   $labgroup = "";
 }
-
 //error_log("Group by $groupby\n",3,"/tmp/log");
 foreach ($_REQUEST as $key => $val) {
 	error_log("Key $key : $val\n",3,"/tmp/log");
 }
 date_default_timezone_set ('America/New_York');
+//$groupby = 'wday';
+//$labgroup = 'Giribet_Lab';
+//$user = "";
 $userstr = "";
 
 if ($labgroup == "" && $user == "") {
@@ -56,7 +58,7 @@ if ($labgroup == "" && $user == "") {
       
       print $date->format('Y-m-d H:i:s') . "\t" . (int)($data['count']) . "\n";
     }
- } else {
+ } else if ($groupby == "mday") {
     $qstr = "select submit_mday,submit_month,submit_year,count(*) count from finish_job group by 3,2,1";
 
     $rawdata = mysql_magic($qstr);
@@ -71,7 +73,30 @@ if ($labgroup == "" && $user == "") {
       
       print $date->format('Y-m-d H:i:s') . "\t" . (int)($data['count']) . "\n";
     }
-}
+ } else if ($groupby == "wday") {
+    $qstr = "select submit_wday,count(*) count from finish_job group by submit_wday";
+
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+    
+      $day   = $data['submit_wday']+1;
+      
+      print "$day\t" . (int)($data['count']) . "\n";
+    }
+} else if ($groupby == "hour") {
+
+    $qstr = "select submit_hour,count(*) count from finish_job group by submit_hour";
+
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+    
+      $day   = $data['submit_hour']+1;
+      
+      print "$day\t" . (int)($data['count']) . "\n";
+    }
+ }
 } else if ($labgroup != ""  && $user == "") {
 
   $users = get_users_by_labgroup($labgroup);
@@ -97,7 +122,7 @@ if ($labgroup == "" && $user == "") {
       print $date->format('Y-m-d H:i:s') . "\t" . (int)($data['count']) . "\n";
     }
 
-  } else {
+  } else if ($groupby == "mday"){
     $qstr = "select submit_mday,submit_month,submit_year,
                         count(*) count
 			from finish_job
@@ -115,7 +140,33 @@ if ($labgroup == "" && $user == "") {
       print $date->format('Y-m-d H:i:s') . "\t" . (int)($data['count']) . "\n";
       
     }
+  } else if ($groupby == "wday") {
+    $qstr = "select submit_wday,
+                        count(*) count
+                        from finish_job
+                            where user_internal_id in $userstr group by submit_wday ";
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+      $day   = $data['submit_wday']+1;
+      
+      print $day . "\t" . (int)($data['count']) . "\n";
+    }     
+   } else if ($groupby == "hour") {
+    $qstr = "select submit_hour,
+                        count(*) count
+                        from finish_job
+                            where user_internal_id in $userstr group by submit_hour ";
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+      $hour   = $data['submit_hour']+1;
+      
+      print $hour . "\t" . (int)($data['count']) . "\n";
+      
+    }
   }
+  
 } else {
   
   if (isset($user) && $user != "") {
@@ -160,6 +211,31 @@ if ($labgroup == "" && $user == "") {
       $date = new DateTime("$year-$month-$day");
       
       print $date->format('Y-m-d H:i:s') . "\t" . (int)($data['count']) . "\n";
+    }
+   } else if ($groupby == "wday") {
+    $qstr = "select submit_wday,
+                        count(*) count
+                        from finish_job,user
+ 			    where finish_job.user_internal_id = user.user_internal_id $userstr group by submit_wday";
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+      $day   = $data['submit_wday']+1;
+      
+      print $day . "\t" . (int)($data['count']) . "\n";
+    }     
+   } else if ($groupby == "hour") {
+    $qstr = "select submit_hour,
+                        count(*) count
+                        from finish_job,user
+ 			where finish_job.user_internal_id = user.user_internal_id $userstr group by submit_hour";
+    $rawdata = mysql_magic($qstr);
+    
+    foreach ($rawdata as $data) {
+      $hour   = $data['submit_hour'];
+      
+      print $hour . "\t" . (int)($data['count']) . "\n";
+      
     }
   }
 }
